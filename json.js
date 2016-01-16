5ea9d86bdcea20faf601a2329b57b9b862007834
james <jpb12@hotmail.co.uk>
2016-01-16 11:15:41 +0000

    Committing files

---
+++ json.js
@@ -0,0 +1,53 @@
+{
+    "timestamp": "16/01/2016 11:00",
+    "name": "Colour",
+    "children": [{
+        "name": "Black",
+		"children": []
+    },{
+        "name": "Blue",
+		"children": [{
+			"name": "Aquamarine",
+			"children": []
+		},{
+			"name": "Cyan",
+			"children": []
+		},{
+			"name": "Navy",
+			"children": []
+		},{
+			"name": "Turquoise",
+			"children": []
+		}]
+    },{
+        "name": "Green",
+		"children": []
+    },{
+        "name": "Purple",
+		"children": [{
+			"name": "Indigo",
+			"children": []
+		},{
+			"name": "Violet",
+			"children": []
+		}]
+    },{
+        "name": "Red",
+		"children": [{
+			"name": "Crimson",
+			"children": []
+		},{
+			"name": "Maroon",
+			"children": []
+		},{
+			"name": "Scarlet",
+			"children": []
+		}]
+    },{
+        "name": "White",
+		"children": []
+    },{
+        "name": "Yellow",
+		"children": []
+    }]
+}
